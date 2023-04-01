95f7b9d568f804c73b4a51225320c5eb83b78da5
Henrique Souza <henrique.asouza@outlook.com>
2023-04-01 12:55:45 -0300

    refactor(home): using fetch with no cache on getData request

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,20 @@
+import { type ProfileDataModel } from "@/domain/models/profile-data-model";
 import HomeClient from "@/presentation/pages/home";
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-import { FirebaseClient } from "@/infra/firebase/firebase-get-data";
 
-async function getData() {
-  const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DB_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-  };
-  const app = initializeApp(firebaseConfig);
-  const database = getDatabase(app);
-  const firebaseClient = new FirebaseClient(database);
+async function getData(): Promise<ProfileDataModel> {
+  const fbUrl = process.env.FIREBASE_DB_URL;
 
-  const res = await firebaseClient.get("profileData");
+  if (!fbUrl) {
+    throw new Error("Firebase URL not defined");
+  }
 
-  return res;
+  const data = await fetch(`${fbUrl}/profileData.json`, { cache: "no-store" });
+
+  return data.json();
 }
 
 export default async function Home() {
-  const data = await getData();
+  const res = await getData();
 
-  return <HomeClient profileData={data} />;
+  return <HomeClient profileData={res} />;
 }
